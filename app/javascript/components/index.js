6ce0d47d46f7b7a616a4b7c3447af76578279cce
Ben Wall <benwall@Bens-MacBook-Air.local>
2019-06-05 13:53:42 -0600

    new react-app repo

---
+++ app/javascript/components/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+class App extends React.Component {
+    render () {
+        return (
+            <h1> hello world</h1>
+        )
+    }
+}
+
+
+export default App
